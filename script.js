fbb0f6de152cb44266bbbbd0ba3e79cf66b81044
pdovan <46533741+pdovan@users.noreply.github.com>
2025-03-29 15:53:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Animation du header
+gsap.from("header", { opacity: 0, y: -50, duration: 1, ease: "power2.out" });
+
+// Animation du titre (h1)
+gsap.from("header h1", { opacity: 0, y: -20, duration: 1, delay: 0.5, ease: "power2.out" });
+
+// Animation du sous-titre (p)
+gsap.from("header p", { opacity: 0, y: 20, duration: 1, delay: 1, ease: "power2.out" });
+
+
+
+
+
